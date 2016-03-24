09bbba11ea34fe94ad2e4f31a8fa46823921db83
Hiroaki Shimono <salmon2073@gmail.com>
2016-03-24 12:38:13 +0900

    Revert "first commit"
    This reverts commit 137e65ee4319f6813b6b89e0b531dec6ccb6b362.

--- myscript.js
+++
@@ -1,5 +0,0 @@
-document.onkeydown = function(){
-	if(event.keyCode == 13 && window.getSelection().toString()!=""&&document.activeElement.tagName!="INPUT"&&document.activeElement.tagName!="TEXTAREA"){
-			window.open("http://www.google.co.jp/search?q="+window.getSelection().toString());
-	}
-}
